e36e51976fe55f4426156a9fbf7326a8e556d0d9
Tahaouad <tahaouad04@gmail.com>
2023-11-09 22:01:16 +0100

    Push

--- jeux_Le_mot_caché.py
+++ jeux_Le_mot_caché.py
@@ -1,5 +1,4 @@
 def MotValide(mot):
-  
   if len(mot) < 4 or len(mot) > 25:
     return False
   for c in mot:
@@ -14,43 +13,24 @@ def convertirCH(mot):
     tab[i] = mot[i]
   return tab
 print(convertirCH('Taha'))
-# Procédure initialiserSolution
 def initialiserSolution(Tcar, n):
-  """
-  Demande à l'utilisateur de saisir le mot à trouver et
-  le remplit dans le tableau Tcar.
-  """
   mot = input("Saisir le mot à trouver (en majuscules, 4-25 caractères): ")
   while not MotValide(mot):
     mot = input("Saisir le mot à trouver (en majuscules, 4-25 caractères): ")
   Tcar = convertirCH(mot)
 
-# Procédure creerEssai
 def creerEssai(Tessai, n):
-  """
-  Remplit le tableau Tessai par le caractère '*'.
-  """
   for i in range(n):
     Tessai[i] = '*'
 
-# Procédure afficherC
 def afficherC(Tcar, n):
-  """
-  Affiche Tcar à l'écran comme une chaîne de caractères.
-  """
   mot = ""
   for c in Tcar:
     mot += c
   print(mot)
 
-# Fonction jouer
 def jouer(Tcar, Tessai, c, n):
-  """
-  Retourne vrai si le caractère c est présent dans la solution et
-  met à jour le tableau Tessai.
-  Le caractère doit remplacé à la bonne place le caractère '*' (pour toutes ses occurrences).
-  Le cas échéant, retourne faux.
-  """
+  
   present = False
   for i in range(n):
     if Tcar[i] == c:
@@ -59,7 +39,6 @@ def jouer(Tcar, Tessai, c, n):
       break
   return present
 
-# Programme principal
 def main():
   n = int(input("Saisir la taille du mot à trouver: "))
   Tcar = ['*'] * n
@@ -80,4 +59,7 @@ def main():
     print("Perdu!")
   else:
     print("Gagné!")
+
+if __name__ == "__main__":
+    main()
 
