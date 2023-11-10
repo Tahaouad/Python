3275f4902af4d06c0b48c604960df8e8c95d648a
Tahaouad <tahaouad04@gmail.com>
2023-11-10 02:34:34 +0100

    Edir

--- jeux_Le_mot_caché.py
+++ jeux_Le_mot_caché.py
@@ -1,65 +1,61 @@
 def MotValide(mot):
-  if len(mot) < 4 or len(mot) > 25:
+    if 4 <= len(mot) <= 25 and all(65 <= ord(c) <= 90 for c in mot):
+        return True
     return False
-  for c in mot:
-    if ord(c) < 65 or ord(c) > 90:
-      return False
-  return True
 
-def convertirCH(mot):
-  
-  tab = ['*'] * len(mot)
-  for i in range(len(mot)):
-    tab[i] = mot[i]
-  return tab
-print(convertirCH('Taha'))
-def initialiserSolution(Tcar, n):
-  mot = input("Saisir le mot à trouver (en majuscules, 4-25 caractères): ")
-  while not MotValide(mot):
+def initialiserSolution(n):
     mot = input("Saisir le mot à trouver (en majuscules, 4-25 caractères): ")
-  Tcar = convertirCH(mot)
+    while not MotValide(mot):
+        mot = input("Saisir le mot à trouver (en majuscules, 4-25 caractères): ")
+    Tcar = list(mot)
+    return Tcar
 
-def creerEssai(Tessai, n):
-  for i in range(n):
-    Tessai[i] = '*'
+def creerEssai(n):
+    return ['*'] * n
 
-def afficherC(Tcar, n):
-  mot = ""
-  for c in Tcar:
-    mot += c
-  print(mot)
+def afficherC(Tcar):
+    mot = ''.join(Tcar)
+    print(mot)
 
-def jouer(Tcar, Tessai, c, n):
-  
-  present = False
-  for i in range(n):
-    if Tcar[i] == c:
-      Tessai[i] = c
-      present = True
-      break
-  return present
+def jouer(Tcar, Tessai, proposition):
+    if len(proposition) != 1:
+        print("Vous devez proposer une seule lettre.")
+        return False
+
+    c = proposition.upper()
+    present = False
+    for i in range(len(Tcar)):
+        if Tcar[i] == c:
+            Tessai[i] = c
+            Tcar[i] = '*'  # Remove the guessed letter from Tcar
+            present = True
+            break
+
+    if present:
+        print("Bon essai!")
+    else:
+        print("Mauvais essai!")
+
+    return present
 
 def main():
-  n = int(input("Saisir la taille du mot à trouver: "))
-  Tcar = ['*'] * n
-  Tessai = ['*'] * n
-  initialiserSolution(Tcar, n)
-  creerEssai(Tessai, n)
-  nbEssais = 0
-  while nbEssais < 7:
-    afficherC(Tessai, n)
-    c = input("Proposer une lettre: ")
-    c = c.upper()
-    if jouer(Tcar, Tessai, c, n):
-      print("Bon essai!")
-    else:
-      print("Mauvais essai!")
-      nbEssais += 1
-  if nbEssais == 7:
-    print("Perdu!")
-  else:
-    print("Gagné!")
+    n = int(input("Saisir la taille du mot à trouver: "))
+    Tcar = initialiserSolution(n)
+    Tessai = creerEssai(n)
+    nbEssais = 0
+
+    while nbEssais < 7:
+        afficherC(Tessai)
+        c = input("Proposer une lettre: ")
+        if not jouer(Tcar, Tessai, c):
+            nbEssais += 1
+
+        if '*' not in Tessai:
+            print("Gagné!")
+            break
+
+    if nbEssais == 7:
+        print("Perdu!")
 
 if __name__ == "__main__":
     main()
-
