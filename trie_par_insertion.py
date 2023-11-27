5a6e66b299dc461362f6d5d96b8916f31bf3ffd0
Tahaouad <tahaouad04@gmail.com>
2023-11-27 04:37:25 +0100

    Variable test

--- trie_par_insertion.py
+++ trie_par_insertion.py
@@ -1,12 +1,3 @@
-def tri_insertion(arr):
-    for i in range(1, len(arr)):
-        cle = arr[i]
-        j = i - 1
-        while j >= 0 and cle < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = cle
-
-liste_a_trier = [12, 11, 13, 5, 6]
-tri_insertion(liste_a_trier)
-print("Liste triée:", liste_a_trier)
+x, y, z = 3, 2, 1
+z, y, x = x, y, z
+print(x, y, z)
