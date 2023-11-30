58bafd8bd0d1a673edd70920bb333e9a265fa6e8
Tahaouad <tahaouad04@gmail.com>
2023-11-30 01:13:13 +0100

    NBr_aliatoire

--- NBr_aliatoire.py
+++ NBr_aliatoire.py
@@ -1,25 +1,43 @@
+import tkinter as tk
+from tkinter import messagebox
 import random
 
-def guess_the_number():
-    print("Bienvenue dans le jeu de devinette !")
-    print("Je pense à un nombre entre 1 et 100. À vous de deviner !")
+class GuessingGame:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Jeu de Devinette")
+        self.master.geometry("300x150")
 
-    # Choisir un nombre aléatoire entre 1 et 100
-    number_to_guess = random.randint(1, 100)
+        self.number_to_guess = random.randint(1, 100)
+        self.attempts = 0
 
-    attempts = 0
+        self.label = tk.Label(self.master, text="Devinez le nombre:")
+        self.label.pack(pady=10)
 
-    while True:
-        user_guess = int(input("Entrez votre devinette : "))
-        attempts += 1
+        self.entry = tk.Entry(self.master)
+        self.entry.pack(pady=5)
 
-        if user_guess < number_to_guess:
-            print("Le nombre à deviner est plus grand. Essayez à nouveau.")
-        elif user_guess > number_to_guess:
-            print("Le nombre à deviner est plus petit. Essayez à nouveau.")
+        self.guess_button = tk.Button(self.master, text="Devinez", command=self.check_guess)
+        self.guess_button.pack(pady=10)
+
+    def check_guess(self):
+        user_guess = int(self.entry.get())
+        self.attempts += 1
+
+        if user_guess < self.number_to_guess:
+            messagebox.showinfo("Résultat", "Le nombre à deviner est plus grand. Essayez à nouveau.")
+        elif user_guess > self.number_to_guess:
+            messagebox.showinfo("Résultat", "Le nombre à deviner est plus petit. Essayez à nouveau.")
         else:
-            print(f"Félicitations ! Vous avez deviné le nombre en {attempts} tentatives.")
-            break
+            messagebox.showinfo("Félicitations", f"Vous avez deviné le nombre en {self.attempts} tentatives.")
+            self.reset_game()
+
+    def reset_game(self):
+        self.number_to_guess = random.randint(1, 100)
+        self.attempts = 0
+        self.entry.delete(0, tk.END)
 
 if __name__ == "__main__":
-    guess_the_number()
+    root = tk.Tk()
+    game = GuessingGame(root)
+    root.mainloop()
