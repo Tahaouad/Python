07cb195d667d61ae535d64b54557a61add1c2948
Tahaouad <tahaouad04@gmail.com>
2023-11-30 22:07:53 +0100

    code snake

--- jeu.py
+++ jeu.py
@@ -1,61 +1,67 @@
 import pygame
+import sys
 import time
 import random
 
 pygame.init()
 
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 102)
-BLACK = (0, 0, 0)
-RED = (213, 50, 80)
-GREEN = (0, 255, 0)
-BLUE = (50, 153, 213)
+# Définir les couleurs
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
 
-DIS_WIDTH = 800
-DIS_HEIGHT = 600
-DIS = pygame.display.set_mode((DIS_WIDTH, DIS_HEIGHT))
-pygame.display.set_caption('Snake Game')
+# Définir la taille de l'écran
+dis_width = 600
+dis_height = 400
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game by Your Name')
 
-CLOCK = pygame.time.Clock()
+# Définir la vitesse du serpent
+snake_block = 10
+snake_speed = 15
 
-SNAKE_BLOCK = 10
-SNAKE_SPEED = 10
+# Définir la police et la taille du texte
+font_style = pygame.font.SysFont(None, 30)
 
-FONT_STYLE = pygame.font.SysFont(None, 50)
-
-
+# Fonction pour dessiner le serpent sur l'écran
 def our_snake(snake_block, snake_list):
-    for segment in snake_list:
-        pygame.draw.rect(DIS, BLACK, [segment[0], segment[1], snake_block, snake_block])
+    for x in snake_list:
+        pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
 
+# Fonction pour afficher le score
+def Your_score(score):
+    value = font_style.render("Your Score: " + str(score), True, white)
+    dis.blit(value, [0, 0])
 
-def message(msg, color):
-    mesg = FONT_STYLE.render(msg, True, color)
-    DIS.blit(mesg, [DIS_WIDTH / 6, DIS_HEIGHT / 3])
-
-
-def game_loop():
+# Fonction principale pour exécuter le jeu
+def gameLoop():
     game_over = False
     game_close = False
 
-    x1 = DIS_WIDTH / 2
-    y1 = DIS_HEIGHT / 2
+    # Position initiale du serpent
+    x1 = dis_width / 2
+    y1 = dis_height / 2
 
+    # Changement initial de la position du serpent
     x1_change = 0
     y1_change = 0
 
-    snake_list = []
-    length_of_snake = 1
+    # Initialiser la longueur du serpent
+    snake_List = []
+    Length_of_snake = 1
 
-    foodx = round(random.randrange(0, DIS_WIDTH - SNAKE_BLOCK) / 10.0) * 10.0
-    foody = round(random.randrange(0, DIS_HEIGHT - SNAKE_BLOCK) / 10.0) * 10.0
+    # Position initiale de la pomme
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
     while not game_over:
 
         while game_close:
-            DIS.fill(BLUE)
-            message("You Lost! Press Q-Quit or C-Play Again", RED)
-            our_snake(SNAKE_BLOCK, snake_list)
+            dis.fill(blue)
+            Your_score(Length_of_snake - 1)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -64,56 +70,63 @@ def game_loop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        game_loop()
+                        gameLoop()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x1_change = -SNAKE_BLOCK
+                    x1_change = -snake_block
                     y1_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    x1_change = SNAKE_BLOCK
+                    x1_change = snake_block
                     y1_change = 0
                 elif event.key == pygame.K_UP:
-                    y1_change = -SNAKE_BLOCK
+                    y1_change = -snake_block
                     x1_change = 0
                 elif event.key == pygame.K_DOWN:
-                    y1_change = SNAKE_BLOCK
+                    y1_change = snake_block
                     x1_change = 0
 
-        if x1 >= DIS_WIDTH or x1 < 0 or y1 >= DIS_HEIGHT or y1 < 0:
+        # Vérifier si le serpent atteint les bords de l'écran
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
 
+        # Mettre à jour la position du serpent
         x1 += x1_change
         y1 += y1_change
+        dis.fill(blue)
+        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_List.append(snake_head)
 
-        DIS.fill(BLUE)
-        pygame.draw.rect(DIS, GREEN, [foodx, foody, SNAKE_BLOCK, SNAKE_BLOCK])
+        # Supprimer les éléments du serpent s'il est trop long
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
 
-        snake_head = [x1, y1]
-        snake_list.append(snake_head)
-
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
-
-        for segment in snake_list[:-1]:
-            if segment == snake_head:
+        # Vérifier la collision avec soi-même
+        for x in snake_List[:-1]:
+            if x == snake_head:
                 game_close = True
 
-        our_snake(SNAKE_BLOCK, snake_list)
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
         pygame.display.update()
 
+        # Générer une nouvelle pomme si le serpent la mange
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, DIS_WIDTH - SNAKE_BLOCK) / 10.0) * 10.0
-            foody = round(random.randrange(0, DIS_HEIGHT - SNAKE_BLOCK) / 10.0) * 10.0
-            length_of_snake += 1
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
 
-        CLOCK.tick(SNAKE_SPEED)
+        # Contrôler la vitesse du serpent
+        pygame.time.Clock().tick(snake_speed)
 
     pygame.quit()
     quit()
 
-
-game_loop()
+gameLoop()
